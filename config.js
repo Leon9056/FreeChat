db89fc04854b049734089fc01063888d8f90ba8f
Leon9056 <leogamercraft12@gmail.com>
2026-09-07 19:37:09 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // URL pública do servidor Render (não coloque segredos aqui).
-window.CONVERSA_VERSION = "1.5.0";
+window.CONVERSA_VERSION = "1.6.0";
 window.SIGNALING_URL = "https://conversa-live-server.onrender.com";
